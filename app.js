40d0268d90bf7916969f4446124b32ce719be84f
shaziabatool90 <batoolshazia90@gmail.com>
2025-06-20 14:15:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.tasty_recipe .autoplay').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+})
